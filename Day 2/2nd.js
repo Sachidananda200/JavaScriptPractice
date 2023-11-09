c3b3fcda880c014bff40d4e50fe07f632f0ac6bd
Sachidanandamallick <150367719+Sachidanandamallick@users.noreply.github.com>
2023-11-09 22:05:02 +0530

    Create 2nd.js

---
+++ Day 2/2nd.js
@@ -0,0 +1,13 @@
+// Strings
+let str = "JavaScript";
+console.log(str.length);         
+console.log(str.toUpperCase());   
+console.log(str.indexOf("Script")); 
+
+// Arrays
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits.length);       
+console.log(fruits[1]);           
+
+fruits.push("date");              
+fruits.pop();
