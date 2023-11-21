c0252341bb4b1370ad6d2367ce9cfb5ec81936a3
Your Name <sachidananda>
2023-11-21 15:58:30 +0530

    add 2nd.js

---
+++ Day 9/2nd.js
@@ -0,0 +1,117 @@
+
+// function printValues(callback) {
+//        let values = [1, 2, 3, 4, 5];
+    
+   
+//     for (let i = 0; i < values.length; i++) {
+   
+//       callback(values[i]);
+//     }
+//   }
+  
+  
+//   printValues(function(value) {
+  
+//     console.log(value);
+//   });
+  
+// const arr = [1, 2, 3, 4, 5];
+
+// const arr1 = arr.map((num) => {
+//   return num * 10;
+// });
+
+// console.log(arr1);
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const arr1 = arr.map((num) => num * 10);
+
+// console.log(arr1);
+
+// function example() {
+//     if (true) {
+//       var x = 5;
+//     }
+//     console.log(x); 
+//   }
+  
+
+
+// function sayHello(message) {
+//     console.log(message + ' ' + this.name);
+//   }
+  
+//   const person = { name: 'sachi' };
+  
+//   sayHello.call(person, 'Hello'); 
+
+
+// function sayHello(message) {
+//     console.log(message + ' ' + this.name);
+//   }
+  
+//   const person = { name: 'sachi' };
+  
+//   sayHello.apply(person, ['Hello']); 
+
+
+//   function sayHello(message) {
+//     console.log(message + ' ' + this.name);
+//   }
+  
+//   const person = { name: 'sachi' };
+  
+//   const sayHelloToJohn = sayHello.bind(person);
+//   sayHelloToJohn('Hello'); 
+  
+  
+
+//   const numbers = [1, 2, 3, 4, 5];
+
+// const sum = numbers.reduce((accumulator, currentValue) => {
+//   return accumulator + currentValue;
+// }, 0);
+
+// console.log(sum); 
+
+
+
+// function outerFunction() {
+//     let outerVariable = 'out side';
+  
+    
+//     function innerFunction() {
+//             console.log(outerVariable);
+//     }
+  
+   
+//     return innerFunction;
+//   }
+  
+  
+//   const closure = outerFunction();
+  
+  
+//   closure();  
+
+
+
+
+// function printAll() {
+//     const a = [1, 2, 3, 4, 5];
+  
+//     function printValues() {
+//       for (let i = 0; i < a.length; i++) {
+        
+//         console.log(`${a[i]}`);
+//       }
+//     }
+  
+    
+//     printValues();
+//   }
+  
+//   printAll();
+  
+  
